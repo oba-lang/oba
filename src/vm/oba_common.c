4443d95e1af4848b413b01ba38a2c55427848987
Kendal Harland <3987220+kendalharland@users.noreply.github.com>
2020-10-31 22:34:57 -0400

    Fix example formatting (#53)
    Print stack traces on error
    
    Support panicking
    
    Support optional values
    
    Test mod option
    
    Add function to assert arity in native
    
    Broken state: need to fix panic
    
    Define macro for creating buffers
    
    Complete buffer macros
    
    WIP fix format
    
    Make it easier to report errors from natives
    
    Create a runtime error macro
    
    Add common error handlers

--- src/vm/oba_common.c
+++ src/vm/oba_common.c
@@ -8,7 +8,6 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 
   void* result = realloc(pointer, newSize);
   // Fail fast if we can't get the requested memory.
-  if (result == NULL)
-    exit(1);
+  if (result == NULL) exit(1);
   return result;
 }
